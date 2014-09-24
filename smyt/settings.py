f69b9cfc69f9808d3fefda8c44a522b19668f2ba
Kovalenko Roman <kovalenkoor@gmial.com>
2014-09-24 22:42:19 +0600

    settings modified

--- smyt/settings.py
+++ smyt/settings.py
@@ -83,6 +83,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
